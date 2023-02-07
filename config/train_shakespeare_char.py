0f85e999c0af797cfbe963055ba78a96391a0322
Alexander Edwards <alex@alexedw.com>
2023-02-07 22:09:42 +1100

    Added MLP

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -14,7 +14,7 @@ wandb_project = 'shakespeare-char'
 wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare_char'
-batch_size = 64
+batch_size = 1
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
@@ -24,7 +24,7 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
+max_iters = 300
 lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
@@ -32,5 +32,5 @@ beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
-# device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
+device = 'cpu'  # run on cpu only
+compile = False # do not torch compile the model
