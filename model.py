9f31fd3490a898bbce4c25cc0c05074ed464f66e
Alexander Edwards <alex@alexedw.com>
2023-02-16 11:17:46 +1100

    nice work

--- model.py
+++ model.py
@@ -41,16 +41,16 @@ class RotaryEmbedding(torch.nn.Module):
         super().__init__()
         self.rotary_ndims = dim
         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
-        self.register_buffer("inv_freq", inv_freq)
 
         # Build here to make `torch.jit.trace` work.
         self.max_seq_len_cached = max_position_embeddings
-        t = torch.arange(self.max_seq_len_cached, device=self.inv_freq.device, dtype=self.inv_freq.dtype)
-        freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+        t = torch.arange(self.max_seq_len_cached)
+        freqs = torch.einsum("i,j->ij", t, inv_freq)
         # Different from paper, but it uses a different permutation in order to obtain the same calculation
         emb = torch.cat((freqs, freqs), dim=-1)
-        self.cos_cached = emb.cos()[None, None, :, :]
-        self.sin_cached = emb.sin()[None, None, :, :]
+        
+        self.register_buffer("cos_cached", emb.cos()[None, None, :, :])
+        self.register_buffer("sin_cached", emb.sin()[None, None, :, :])
 
     def rotate_half(self, x):
         """Rotates half the hidden dims of the input."""
